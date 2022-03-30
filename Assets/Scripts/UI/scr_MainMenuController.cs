d4e819bb148e008c0a46f5dcb6bf6ab57ab6403d
arnnau <arnas.naujalis@ktu.edu>
2022-03-30 22:47:43 +0300

    Added simple main menu (will add style later)

---
+++ Assets/Scripts/UI/scr_MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        scr_Scenes.LoadNextScene();
+    }
+    public void ExitGame()
+    {
+        scr_Scenes.ExitGame();
+    }
+}
---
+++ Assets/Scripts/UI/scr_Scenes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_Scenes
+{
+    public static void RestartScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public static void LoadNextScene() {
+        LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public static void LoadPreviousScene() {
+        LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public static void ExitGame() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    private static void LoadScene(int buildIndex) {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
